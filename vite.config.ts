e6de415c92942fe5173566a6a14442b70ca5d32d
VicL36 <vicleandro36@gmail.com>
2025-07-05 14:16:14 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,8 @@ export default defineConfig({
     },
   },
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    include: ["replicate", "jspdf"],
+    exclude: ["lucide-react"],
   },
   server: {
     host: '0.0.0.0',
